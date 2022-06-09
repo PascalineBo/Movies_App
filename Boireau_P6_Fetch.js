fc1e240e67ae7c76ac172836381584e0198deb3a
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-06-09 10:10:18 +0200

    ajoût de balises img dynamiques via js

--- Boireau_P6_Fetch.js
+++ Boireau_P6_Fetch.js
@@ -1,4 +1,25 @@
 
+
+function createAllDivMovies(){
+      const GenresList = ["Films_les_mieux_notes", "Comedy", "Crime", "Romance"]
+      for (let i in GenresList) {
+            createDivMovies(GenresList[i]);
+           }
+      }
+
+function createDivMovies(Genre) {
+      const spreadMoviesList = [0,1,2,3,4,5,6]
+      for (let i of spreadMoviesList) {
+            const image = document.createElement("img");
+            image.className = "film";
+            const imageId = "Film";
+            image.id = imageId + i;
+            const idComedy = document.getElementById(Genre);
+            idComedy.appendChild(image);
+          }
+      }
+
+
 function fetchMoviesData() {
     fetch("http://localhost:8000/api/v1/titles/?page=1")
       .then(response => response.json())
@@ -8,15 +29,34 @@ function fetchMoviesData() {
 function getMoviesResults(data) {
     console.log(data);
     console.log(data.results);
-    let getImageUrl = data.results[0].image_url;
+    const getImageUrl = data.results[0].image_url;
     console.log(getImageUrl);
     const affiche_Meilleur_film = document.getElementById('img_Meilleur_film');
     affiche_Meilleur_film.src = getImageUrl;
     }
 
-window.onload = function() {
-      fetchMoviesData() 
-          };
+//fonctions à refactoriser plus tard:
 
+function ComedyMoviesData() {
+    fetch("http://localhost:8000/api/v1/titles/?genre=Comedy&page=1&sort_by=-imdb_score")
+      .then(response2 => response2.json())
+      .then(data2 => getComedyMoviesResults(data2));
+    }
+
+function getComedyMoviesResults(data2) {
+    console.log(data2);
+    console.log(data2.results);
+    // rapatrie l'url du film le mieux noté récemment
+    const getImageUrl_2 = data2.results[0].image_url;
+    console.log(getImageUrl_2);
+    const affiche_Comedy = document.getElementById('img_Comedy1');
+    affiche_Comedy.src = getImageUrl_2;
+    }
+
+window.onload = function() {
+      fetchMoviesData(); 
+      ComedyMoviesData();
+      createAllDivMovies();
+    };
 
       
