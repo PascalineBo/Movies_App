2ab965fe86caf4391ed1da36f772e100c01f4e0b
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-06-15 10:23:47 +0200

    Travail sur l'affichage des données du meilleur film

--- Boireau_P6_New_Fetch.js
+++ Boireau_P6_New_Fetch.js
@@ -10,6 +10,7 @@ createAllDivMovies();
 for (let i = 1; i < GenresList.length; i++){
       console.log(GenresList[i]);
       fetchDataByGenre(GenresList[i],1);
+      arrowsOther(GenresList[i]);
 }
 
 //récupère la première page des films les mieux notés
@@ -20,48 +21,95 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
                     const billboardGenre = document.getElementById("Film"+ i + "Films_les_mieux_notes");
                     billboardGenre.src = getImageUrl_2
                   }
-                  //affichage des éléments du meilleur film
+                  //affichage des éléments du meilleur film:
+                  //titre
                     const getMovieTitle = data.results[0].title;
                     const bestMovieTitle = document.getElementById('Meilleur_film');
-                    bestMovieTitle.innerHTML = "<h2>Meilleur Film: "+getMovieTitle+"</h2><img id=\"img_Meilleur_film\" alt = \"affiche du Meilleur Film\">";
+                    bestMovieTitle.innerHTML = "<h2>Meilleur Film: "+getMovieTitle+"</h2><div id=\"Meilleur_film_content\"><img id=\"img_Meilleur_film\" alt = \"affiche du Meilleur Film\"></div>"; 
+                    //affiche
                     const getImageUrl = data.results[0].image_url;
                     const billboardBestMovie = document.getElementById('img_Meilleur_film');
                     billboardBestMovie.src = getImageUrl;
+                      //cherche le lien vers la page où se trouve le résumé:
+                      const movieURL = data.results[0].url;
+                      console.log(movieURL);
+                    fetch(""+movieURL+"")
+                        .then(response2 => response2.json())
+                        .then(data2 => {
+                               console.log(data2);
+                               movieSumUP = data2.description;
+                               //met un résumé du meilleur film
+                               const sumUp = document.createElement("p");
+                               console.log(sumUp);
+                               sumUp.innerHTML = movieSumUP;
+                               const meilleurFilmContent = document.getElementById("Meilleur_film_content");
+                               meilleurFilmContent.appendChild(sumUp);
+                               meilleurFilmContent.style.display = "flex"; 
+                              })                    
                   })
-//récupère les pages suivantes des films les mieux notés
+//récupère la page suivantes des films les mieux notés
 fetchBestMovies(2);
 
-// flèches du carrousel
+//met des flèches de défilement aux films les mieux notés
+arrowsBest('bestRated');
 
-//flèche de droite
-const bestRatedArrow = document.getElementById('fwd_bestRated');
-let bestRatedArrowClick = 2;
-let bestRatedArrowClickList = [];
-let clickIndex = 0;
 
-bestRatedArrow.addEventListener("click",function() {
-      fetchBestMoviesFirstPart(++bestRatedArrowClick);
-      fetchBestMovies(++bestRatedArrowClick + 1);
-      bestRatedArrowClickList.push(++clickIndex);
-      console.log(bestRatedArrowClickList);
-})
 
-//flèche de gauche
-const bestRatedBackArrow = document.getElementById('back_bestRated');
+// flèches de défilement pour les films les mieux notés:
+function arrowsBest(Genre){
+      //flèche de droite
+      const fwdArrow = document.getElementById("fwd_"+Genre);
+      let fwdArrowClick = 2;
 
-bestRatedBackArrow.addEventListener("click",function(noRightClick) {
-      if (bestRatedArrowClickList.length == 0) {
-      noRightClick.preventDefault();      
-} else {
-      let backIndex = bestRatedArrowClickList.pop();
-      fetchBestMoviesFirstPart(backIndex);
-      console.log(backIndex);
-      fetchBestMovies(backIndex + 1);
-      console.log(bestRatedArrowClickList);
+      fwdArrow.addEventListener("click",function() {
+            fetchBestMoviesFirstPart(++fwdArrowClick);
+            console.log("fwdArrowClick = "+fwdArrowClick);
+            fetchBestMovies(fwdArrowClick + 1);
+      })
+
+      //flèche de gauche
+      const backArrow = document.getElementById('back_'+Genre);
+
+      backArrow.addEventListener("click",function(noRightClick) {
+            if (fwdArrowClick == 1) {
+            noRightClick.preventDefault();      
+      } else {
+            fwdArrowClick = fwdArrowClick - 1; //diminue le numéro de page consultée
+            console.log("fwdArrowClick = "+fwdArrowClick);
+            fetchBestMoviesFirstPart(fwdArrowClick);
+            console.log(fwdArrowClick);
+            fetchBestMovies(fwdArrowClick + 1);
+      }
+      })
 }
-})
 
+// flèches de défilement pour les films des autres catégories:
 
+function arrowsOther(Genre){
+      //flèche de droite
+      const otherFwdArrow = document.getElementById("fwd_"+Genre);
+      let otherFwdArrowClick = 2;
+
+      otherFwdArrow.addEventListener("click",function() {
+            fetchDataByGenre(Genre,++otherFwdArrowClick);
+            console.log("otherfwdArrowClick = "+otherFwdArrowClick);
+      })
+
+      //flèche de gauche
+      const otherBackArrow = document.getElementById('back_'+Genre);
+
+      otherBackArrow.addEventListener("click",function(noRightClick) {
+            if (otherFwdArrowClick == 1) {
+            noRightClick.preventDefault();      
+      } else {
+            otherFwdArrowClick = otherFwdArrowClick - 1; //diminue le numéro de page consultée
+            console.log("otherfwdArrowClick = "+otherFwdArrowClick);
+            const pageNumber = otherFwdArrowClick;
+            fetchDataByGenre(Genre, pageNumber);
+            console.log(otherFwdArrowClick);
+      }
+      })
+}
 
 //récupération des films les mieux notés toutes catégories confondues 2e page
 function fetchBestMovies(page){
