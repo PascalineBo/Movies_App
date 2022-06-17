9d3d21586ee3e56b147ad173b46610e1130622a0
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-06-17 16:28:47 +0200

    Mise en page meilleur film - et sa modale!

--- Boireau_P6_New_Fetch.js
+++ Boireau_P6_New_Fetch.js
@@ -1,9 +1,10 @@
 
 
-
-
 const GenresList = ["Films_les_mieux_notes", "Comedy", "Crime", "Romance"]
 
+//fonction qui activent la modale
+
+//
 
 createAllDivMovies();
 
@@ -30,6 +31,7 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
                     const getImageUrl = data.results[0].image_url;
                     const billboardBestMovie = document.getElementById('img_Meilleur_film');
                     billboardBestMovie.src = getImageUrl;
+                    billboardBestMovie.className = "floatingIMG";
                       //cherche le lien vers la page où se trouve le résumé:
                       const movieURL = data.results[0].url;
                       console.log(movieURL);
@@ -37,14 +39,83 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
                         .then(response2 => response2.json())
                         .then(data2 => {
                                console.log(data2);
-                               movieSumUP = data2.description;
+                               const meilleurFilmContent = document.getElementById("Meilleur_film_content");
+                               const divBestMovieRightSide= document.createElement("div");
+                               meilleurFilmContent.appendChild(divBestMovieRightSide);
+                               //crée la modale du meilleur film
+                               const modalContainerDiv = document.createElement("div");
+                               divBestMovieRightSide.appendChild(modalContainerDiv);
+                               modalContainerDiv.className = "modal-container";
+                               const overlay = document.createElement("div");
+                               modalContainerDiv.appendChild(overlay);
+                               overlay.className = "overlay modal-trigger";
+                               const dialog = document.createElement("div");
+                               modalContainerDiv.appendChild(dialog);
+                               dialog.className = "modal";
+                               const closeModal = document.createElement("button");
+                               dialog.appendChild(closeModal);
+                               closeModal.className = "close-modal modal-trigger";
+                               closeModal.innerHTML = "x";
+                               // va chercher et injecte les données du film dans la modale:
+                               const movieImage = document.createElement("img");
+                               movieImage.src = data2.image_url;
+                               movieImage.className = "floatingIMG"
+                               dialog.appendChild(movieImage);
+                               const movieTitle = document.createElement("h2");
+                               dialog.appendChild(movieTitle);
+                               movieTitle.innerHTML = data2.title;
+                               const pubDate = document.createElement("h4");
+                               dialog.appendChild(pubDate);
+                               pubDate.innerHTML = "Rated: "+data2.rated+"     - "+data2.genres;
+                               const IMDBScore = document.createElement("h5");
+                               dialog.appendChild(IMDBScore);
+                               IMDBScore.innerHTML = "Score IMDB: "+data2.imdb_score;
+                               const director = document.createElement("h5");
+                               dialog.appendChild(director);
+                               director.innerHTML = "Film Director: "+data2.directors[0];
+                               const actors = document.createElement("h5");
+                               dialog.appendChild(actors);
+                               actors.innerHTML = "Actors: "+data2.actors[0];
+                               for (let i = 1; i < data2.actors.length; i++){
+                                    actors.innerHTML += ", "+data2.actors[i];
+                               }
+                               const duration = document.createElement("h5");
+                               dialog.appendChild(duration);
+                               duration.innerHTML = "Duration: "+data2.duration+" min";
+                               const country = document.createElement("h5");
+                               dialog.appendChild(country);
+                               country.innerHTML = "Country: "+data2.countries[0];
+                               if (data2.countries.length > 1) {
+                                    for (let i = 1; i < data2.countries.length; i++){
+                                          countries.innerHTML += ", "+data2.countries[i];
+                                    }
+                                    } else {};
+                              const boxOffice = document.createElement("h5");
+                              dialog.appendChild(boxOffice);
+                              const boxOfficeConverted = convertToInternationalCurrencySystem(data2.usa_gross_income);
+                              boxOffice.innerHTML = "Box Office: "+boxOfficeConverted;
+                              const summary = document.createElement("p");
+                              dialog.appendChild(summary);
+                              summary.innerHTML = "Summary: "+data2.long_description;                               
+
+                               //crée le bouton pour la modale du meilleur film
+                               const bestButton = document.createElement("button");
+                               bestButton.className = "modal-btn modal-trigger";
+                               bestButton.innerHTML = "Plus d'infos";
+                               divBestMovieRightSide.appendChild(bestButton);
                                //met un résumé du meilleur film
                                const sumUp = document.createElement("p");
-                               console.log(sumUp);
-                               sumUp.innerHTML = movieSumUP;
-                               const meilleurFilmContent = document.getElementById("Meilleur_film_content");
-                               meilleurFilmContent.appendChild(sumUp);
-                               meilleurFilmContent.style.display = "flex"; 
+                               sumUp.innerHTML = data2.description;
+                               sumUp.id = "bestMovieParagraph";
+                               divBestMovieRightSide.appendChild(sumUp);
+                               divBestMovieRightSide.style.display = "block"; 
+                               //active la modale si clic:
+                               const modalContainer = document.querySelector(".modal-container");
+                               const modalTriggers = document.querySelectorAll(".modal-trigger");
+                               modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+                               function toggleModal(){
+                                    modalContainer.classList.toggle("active")
+                                  }
                               })                    
                   })
 //récupère la page suivantes des films les mieux notés
@@ -100,7 +171,7 @@ function arrowsOther(Genre){
 
       otherBackArrow.addEventListener("click",function(noRightClick) {
             if (otherFwdArrowClick == 1) {
-            noRightClick.preventDefault();      
+            noRightClick.preventDefault(); //cas où le carrousel n'a pas encore défilé vers la droite     
       } else {
             otherFwdArrowClick = otherFwdArrowClick - 1; //diminue le numéro de page consultée
             console.log("otherfwdArrowClick = "+otherFwdArrowClick);
@@ -214,3 +285,11 @@ function createDivMovies(Genre) {
             idComedy.appendChild(image);
           }
       }
+
+function convertToInternationalCurrencySystem (labelValue) {
+      // Six Zeroes for Millions 
+      return Math.abs(Number(labelValue)) >= 1.0e+6
+      ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1) + " M"
+      : Math.abs(Number(labelValue));
+    }
+
