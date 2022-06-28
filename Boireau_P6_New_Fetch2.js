12a81c94784fd85e02dc3ce0fe343cc3f80297f8
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-06-28 15:05:59 +0200

    travail sur les modales du Carrousel avec nouvelle fonction2

---
+++ Boireau_P6_New_Fetch2.js
@@ -0,0 +1,215 @@
+const GenresList = ["Films_les_mieux_notes", "Comedy", "Crime", "Romance"]
+
+//stockage des données des 20 premiers films récupérés de l'API pour chaque Genre:
+const dataGenreList = [0,fetchDataByGenre(GenresList[1]),fetchDataByGenre(GenresList[2]),fetchDataByGenre(GenresList[3])]
+
+// crée les balises pour les films affichés:
+
+createAllDivMovies();
+
+//va chercher les images à afficher dans le carrousel des meilleurs films:
+
+fetchDataBestMovies();
+
+//va chercher les images à afficher dans les carrousels des catégories:
+
+display()
+
+function display(){
+for (let i = 1; i < GenresList.length; i++){
+      fetchDataByGenre(GenresList[i]);
+}
+}
+
+
+// crée les balises pour les films affichés pour chaque Genre déclaré en variable:
+
+function createAllDivMovies() {
+    for (let i in GenresList) {
+          createDivMovies(GenresList[i]);
+         }
+    }
+
+// crée les balises pour les films affichés:
+
+function createDivMovies(Genre) {
+    const spreadMoviesList = [0,1,2,3,4,5,6];
+    for (let i of spreadMoviesList) {
+          // crée div pour les modales:
+          const modalContainerDiv = document.createElement("div");
+          modalContainerDiv.className = "modal-container";
+          modalContainerDiv.classList.add("modal-container"+Genre+i);
+          const modaleId = "Modale";
+          modalContainerDiv.id = modaleId + i + Genre;
+          // crée balises pour les images + trigger pour les modales:
+          const image = document.createElement("img");
+          image.className = "film modal-trigger"+Genre+i;
+          const imageId = "Film";
+          image.id = imageId + i + Genre;
+          const idComedy = document.getElementById(Genre);
+          idComedy.appendChild(modalContainerDiv);
+          idComedy.appendChild(image);
+        }
+    }
+
+// affiche les 7 premières images des carrousels:
+function showSevenFirstMoviePics (Genre, imageURLsList, page = 0) {
+    for (let j = 0; j < 7; j++){
+    if (document.getElementById("Film"+ j + Genre) != null) {
+          console.log("Film"+ j + Genre);
+          const billboardGenre = document.getElementById("Film" + j + Genre);
+          billboardGenre.src = imageURLsList[j + (page)*7][0];
+          billboardGenre.title = imageURLsList[j + (page)*7][1];        
+          } else {
+                continue
+                }
+    }
+}
+
+
+// flèches de défilement pour les carrousels:
+
+function arrows(Genre, imageUrlsList){
+    //flèche de droite
+    const otherFwdArrow = document.getElementById("fwd_"+ Genre);
+    let otherFwdArrowClick = 0;
+
+    otherFwdArrow.addEventListener("click",function(noMoreClick) {
+          otherFwdArrowClick +=1;
+          if (otherFwdArrowClick > 2) {
+                noMoreClick.preventDefault(); //on ne peut cliquer que 2 fois à droite sur ce carrousel; 
+                //(cas où on clique davantage) 
+          } else {
+                //si on clique une ou deux fois à droite:
+                for (let j = 0; j < Math.min(7, imageUrlsList.length - 7*otherFwdArrowClick); j++){ 
+                      if (document.getElementById("Film"+ j + Genre) != null) {
+                      const billboardGenre = document.getElementById("Film"+ j + Genre);
+                      billboardGenre.src = imageUrlsList[7*otherFwdArrowClick + j][0];
+                      billboardGenre.title = imageUrlsList[7*otherFwdArrowClick + j][1];
+                      } else {
+                      continue
+                      }
+                      }
+                console.log("otherfwdArrowClick = "+ otherFwdArrowClick);
+    }
+})
+
+    //flèche de gauche
+    const otherBackArrow = document.getElementById('back_'+ Genre);
+
+    otherBackArrow.addEventListener("click",function(noRightClick) {
+          if (otherFwdArrowClick == 0) {
+          noRightClick.preventDefault(); //cas où le carrousel n'a pas encore défilé vers la droite     
+    } else {
+          otherFwdArrowClick = otherFwdArrowClick - 1; //diminue le numéro de page consultée
+          console.log("otherfwdArrowClick = "+ otherFwdArrowClick);
+          const pageNumber = otherFwdArrowClick;
+          showSevenFirstMoviePics(Genre, imageUrlsList, page = pageNumber);
+          console.log(otherFwdArrowClick);
+    }
+    })
+}
+
+//récupération des films les mieux notés par genre
+
+function fetchDataByGenre(Genre){
+    
+    
+    fetchDataByGenreByPage(URL_1,imageURLsList, Genre) // met les urls image de la première page dans une liste
+    
+    console.log(imageURLsList)
+    
+    //showSevenFirstMoviePics(Genre, imageURLsList, page = 0); // affiche les 7 premières images de la liste dans le carrousel
+                                
+    return imageURLsList;
+    }
+
+// fonction pour la récupération des données des 20 meilleurs films par Genre:
+
+function fetchDataByGenre(Genre) {
+    let imageURLsList = [];
+    let URL = ["http://localhost:8000/api/v1/titles/?genre="+Genre+"&page_size=20&page=1&sort_by=-imdb_score&sort_by=-votes",
+                ];
+    fetch(URL)
+            .then(response => response.json())
+            .then(data => {for (let i = 0; i < data.results.length; i++){
+                    const getImageUrl_2 = data.results[i].image_url;
+                    const getMovieURL = data.results[i].url;
+                    let tupleList = [];
+                    tupleList.push(getImageUrl_2);
+                    tupleList.push(getMovieURL);
+                    imageURLsList.push(tupleList);
+                    if (document.getElementById("Film"+ i + Genre) != null) {
+                    const billboardGenre = document.getElementById("Film"+ i + Genre);
+                    billboardGenre.src = getImageUrl_2;
+                    billboardGenre.title = getMovieURL;
+                    arrows(Genre, imageURLsList);
+                    dispatchEvent (document, 'click', '.modal-container', function() {
+                        openModal()
+                    });
+                    } else {
+                        continue
+                    }
+                  }
+                return imageURLsList
+                }
+            )
+    
+}
+
+// fonction pour la récupération des données des meilleurs films toutes catégories:
+
+function fetchDataBestMovies() {
+    let imageURLsList = [];
+    let URL_1 = ["http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=20&page=1&sort_by=-votes",
+                ];
+    fetch(URL_1)
+            .then(response => response.json())
+            .then(data => {for (let i = 0; i < data.results.length; i++){
+                    const getImageUrl_2 = data.results[i].image_url;
+                    const getMovieURL = data.results[i].url;
+                    let tupleList = [];
+                    tupleList.push(getImageUrl_2);
+                    tupleList.push(getMovieURL);
+                    imageURLsList.push(tupleList);
+                    if (document.getElementById("Film"+ i +"Films_les_mieux_notes") != null) {
+                    const billboardGenre = document.getElementById("Film"+ i +"Films_les_mieux_notes");
+                    billboardGenre.src = getImageUrl_2;
+                    billboardGenre.title = getMovieURL;
+                    arrows("Films_les_mieux_notes", imageURLsList);
+                    } else {
+                        continue
+                    }
+                  }
+                console.log("Best Movie URL List:" + imageURLsList)
+                return imageURLsList
+                }
+            )
+    
+}
+
+//fonction pour le traitement et la présentation de certains chiffres des modales:
+
+function convertToInternationalCurrencySystem (labelValue) {
+      // Six Zeroes for Millions 
+      return Math.abs(Number(labelValue)) >= 1.0e+6
+      ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1) + " M"
+      : Math.abs(Number(labelValue));
+    }
+
+//fonction qui va me servir à limiter les effets de propagation liés à l'évènement "click"
+const dispatchEvent = (Element, type, selector, handler) => {
+    Element.addEventListener(type, (event) => {
+        let el = event.target.closest(selector);
+        if (el) handler.call(el, event); 
+    });
+};
+
+
+
+function openModal() {
+    //active la modale si clic:
+    const getmodalContainer = document.querySelector(".modal-container");
+    //affiche la modale
+    getmodalContainer.classList.toggle("active");
+}
